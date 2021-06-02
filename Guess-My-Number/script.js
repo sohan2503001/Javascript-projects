a9d13468fbe4e7f6f9a37e538813a9c29223b8c0
sohan2503001 <sohanjambhule973@gmail.com>
2021-06-03 01:07:35 +0530

    Added javascript some functions

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,1 +1,36 @@
 'use strict';
+const secretnumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretnumber;
+
+document.querySelector('.check').addEventListener(
+    'click', function () {
+        const guess = Number(document.querySelector('.guess').value);
+        console.log(guess, typeof guess);
+
+        if (!guess) {
+            document.querySelector('.message').textContent = 'No number';
+        } else if (guess === secretnumber) {
+            document.querySelector('.message').textContent = 'correct number';
+        } else if (guess > secretnumber) {
+            if (score > 1) {
+                document.querySelector('.message').textContent = 'Too high';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = 'You lost a game'
+                document.querySelector('.score').textContent = 0;
+            }
+
+        } else if (guess < secretnumber) {
+            if (score > 1) {
+                document.querySelector('.message').textContent = 'Too low';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = 'You lost a game'
+                document.querySelector('.score').textContent = 0;
+            }
+        }
+    }
+)
