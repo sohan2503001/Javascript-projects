db9f1663925308e931a9846445e92a57dc063b1d
sohan2503001 <sohanjambhule973@gmail.com>
2021-06-03 01:33:06 +0530

    Added some DOM CSS

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,17 +1,25 @@
 'use strict';
 const secretnumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretnumber;
+
 
 document.querySelector('.check').addEventListener(
     'click', function () {
         const guess = Number(document.querySelector('.guess').value);
         console.log(guess, typeof guess);
 
+        //input is empty
         if (!guess) {
             document.querySelector('.message').textContent = 'No number';
+
+            //guess is correct
         } else if (guess === secretnumber) {
             document.querySelector('.message').textContent = 'correct number';
+            document.querySelector('.number').textContent = secretnumber;
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem';
+
+            //guess is too high
         } else if (guess > secretnumber) {
             if (score > 1) {
                 document.querySelector('.message').textContent = 'Too high';
@@ -22,6 +30,7 @@ document.querySelector('.check').addEventListener(
                 document.querySelector('.score').textContent = 0;
             }
 
+            //guess is too low
         } else if (guess < secretnumber) {
             if (score > 1) {
                 document.querySelector('.message').textContent = 'Too low';
