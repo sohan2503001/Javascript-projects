649d22c648a69aba4179e52390d3be1f6c66549d
sohan2503001 <sohanjambhule973@gmail.com>
2021-06-03 13:18:05 +0530

    Added all JS functions

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,23 @@
 'use strict';
+const btnsOpen = document.querySelectorAll('.show-modal');
+const btnClose = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const openModel = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpen.length; i++)
+    btnsOpen[i].addEventListener('click', openModel)
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown',)
