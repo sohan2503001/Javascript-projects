91fc456718941c13b30f5ed4934eefe620b28844
sohan2503001 <sohanjambhule973@gmail.com>
2021-06-19 17:38:21 +0530

    Added sorted functionality

--- Bankist/script.js
+++ Bankist/script.js
@@ -63,10 +63,13 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 
 ////Movements display function
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) :
+    movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -177,10 +180,23 @@ btnTransfer.addEventListener('click', function (e) {
   }
 })
 
-btnClose.addEventListener('click', function (e) {
+btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
+  const amount = Number(inputLoanAmount.value);
 
+  if (amount > 0 && currentAccount.movements.some(mov =>
+    mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+
+    //updateUI
+    updateUI(currentAccount);
+
+  }
+})
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
   if (
     inputCloseUsername.value === currentAccount.username &&
     Number(inputClosePin.value) === currentAccount.pin
@@ -191,10 +207,17 @@ btnClose.addEventListener('click', function (e) {
     //delete account
     accounts.splice(index, 1);
 
-    //Hi de UI
+    //Update UI
     containerApp.style.opacity = 0;
   };
   inputCloseUsername.value = inputClosePin.value = '';
+})
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 })
 
 /////////////////////////////////////////////////
