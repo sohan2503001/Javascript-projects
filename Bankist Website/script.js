98c4bd8db7c9f853aa34bfc950a83152a1a70568
sohan2503001 <sohanjambhule973@gmail.com>
2021-07-24 10:05:21 +0530

    Implemented smotth scrolling

--- Bankist Website/script.js
+++ Bankist Website/script.js
@@ -29,3 +29,34 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+////////Implementng Smotth Scrolling///
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y', window.pageXOffset, window.pageYOffset);
+
+  console.log('height/Width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth);
+
+  //Scrolling
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth'
+  });
+});
+
+
+
+
+
